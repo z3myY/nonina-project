9787d02f208613b03fbc50310954c090f66d9057
z3myY <55695078+z3myY@users.noreply.github.com>
2021-11-30 17:08:46 +0100

    script bash para poblar la base de datos heroku

---
+++ heroku_fixtures.sh
@@ -0,0 +1,9 @@
+ php bin/console d:query:sql "INSERT INTO categoria (id, nombre, descripcion) VALUES \
+(1, 'Ruta', 'Ciclismo de carretera'),\
+(2, 'MTB', 'Ciclismo de montaña, XC, XCO, XCM y DH'),\
+(3, 'Ciclocross', NULL),\
+(4, 'Team', 'Relacionado con todas las novedades y actividades del equipo NONINÁ TEAM');"
+
+#password Josgonloz1_
+ php bin/console d:query:sql "INSERT INTO usuario (id, nombre, nick, email, password, roles) VALUES \
+(nextval('usuario_id_seq'), 'admin', 'admin01', 'josgonloz@gmail.com', '$2y$13$5te3clOSipWLW1PEBCQuv.GJxJIPBqUDQQmj5hET11TWbHSdI8Zqq', '[\"ROLE_ADMIN\"]');"
